eb39004a5068d1f1a4d630b74a6efa53ba656af0
Charles Ikulayo <charles.ikulayo@brainbox.group>
2022-06-20 12:25:51 +0200

    schema env change

--- .tina/schema.ts
+++ .tina/schema.ts
@@ -345,7 +345,7 @@ const branch = "https://github.com/charles-brainbox/proofone-website";
 const apiURL =
   process.env.NODE_ENV == "development"
     ? "http://localhost:4001/graphql"
-    : `https://content.tinajs.io/content/${process.env.NEXT_PUBLIC_TINA_CLIENT_ID}/github/${branch}`;
+    : `https://content.tinajs.io/content/${process.env.NEXT_PUBLIC_USE_LOCAL_CLIENT}/github/${branch}`;
 
 export const tinaConfig = defineConfig({
   apiURL,
